5508d17b6b117e7fee608ca2d1c49234250be579
Ma Seo Sen | Tub Xyaas Muas <pyhteam.solutions@gmail.com>
2024-10-20 15:33:21 +0700

    init
    asdasd

---
+++ README.md
@@ -0,0 +1,2 @@
+# Quà Phụ Nữ Việt Nam
+Quà 20/10 Của  🌸✨ Sùng Thị Thu ✨🌸
---
+++ config.js
@@ -0,0 +1,23 @@
+const CONFIG = {
+    introTitle: 'Em yêu à!',
+    introDesc: `Hôm nay là một ngày thật đặc biệt.
+    Một phép tính đơn giản 20/10 đúng không nào? :3
+    Nhưng trước hết anh có một câu hỏi cho em 🥺`,
+    btnIntro: 'Ấn tiếp đi nào :3',
+    title: 'Em yêu anh nhiều không? 😞',
+    desc: 'Có ghét anh không? 🥺',
+    btnYes: 'Vẫn cứ là yêu anh nhiều lắm! 💗💗',
+    btnNo: 'Em ghét anh lắm! 🤬',
+    question: 'Trên thế giới hơn 7 tỉ người, tại sao em lại yêu anh? <3',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    mess: `Anh biết mà 🥰. Anh cũng yêu em nhiều lắm 😘😘.
+    Hôm nay là ngày 20/10, chúc em yêu của anh luôn
+    mạnh khỏe, vui vẻ, gặp nhiều may mắn và hạnh phúc. 
+    Sắp tới thì giảm cân thành công và dễ dàng 💪💪. 
+    Và luôn yêu anh nhé, em yêu 💋💋😊😊`,
+    messDesc: `Tối nay 7h anh qua đón nhé công chúa.\n
+    Chúc em yêu sẽ trở thành người phụ nữ của anh :3`,
+    btnAccept: 'Okiiiii luôn <3',
+    messLink: '/img/hehe.png'
+}
